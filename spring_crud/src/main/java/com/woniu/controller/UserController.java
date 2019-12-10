1ae8f437bfcff480793097771189f9bbec4b136d
showmake <wangyi@163.com>
2019-12-10 18:08:41 +0800

    create findAll

---
+++ spring_crud/src/main/java/com/woniu/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.woniu.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.woniu.pojo.Stu;
+import com.woniu.service.IStuService;
+import com.woniu.service.impl.StuServiceImpl;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private IStuService service;
+	
+	@RequestMapping("/findAll")
+	public String findAll(HttpServletRequest req) {
+		System.out.println("UserController.findAll()");
+		req.setAttribute("stuList", service.findAll());
+		return "list";
+	}
+	
+	
+}
---
+++ spring_crud/src/main/java/com/woniu/service/IStuService.java
@@ -0,0 +1,9 @@
+package com.woniu.service;
+
+import java.util.List;
+
+import com.woniu.pojo.Stu;
+
+public interface IStuService {
+	List<Stu> findAll();
+}
